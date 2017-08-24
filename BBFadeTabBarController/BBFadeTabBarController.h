a09ed5f4f95a880e9f187e780555a5fe9a4c2095
Berger <contact@bergerbytes.com>
2017-08-24 12:14:13 -0500

    Initial Commit

---
+++ BBFadeTabBarController/BBFadeTabBarController.h
@@ -0,0 +1,19 @@
+//
+//  BBFadeTabBarController.h
+//  BBFadeTabBarController
+//
+//  Created by Michael Berger on 8/24/17.
+//  Copyright © 2017 BergerBytes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BBFadeTabBarController.
+FOUNDATION_EXPORT double BBFadeTabBarControllerVersionNumber;
+
+//! Project version string for BBFadeTabBarController.
+FOUNDATION_EXPORT const unsigned char BBFadeTabBarControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BBFadeTabBarController/PublicHeader.h>
+
+
